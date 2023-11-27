ec18f856d5444fa752754f6ff3524e7bc63dd633
Kaustav Chanda <kstvkmrchanda2@gmail.com>
2023-11-27 23:53:20 +0530

    Initialize Discuss Project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import Providers from '@/app/providers';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="container mx-auto px-12">{children}</div>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
